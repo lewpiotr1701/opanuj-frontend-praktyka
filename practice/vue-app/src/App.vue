19b9cb9be6bdcdd2b8f76cad29eaa2c0afc42de9
Piotr <lewczykpiotr@gmail.com>
2024-02-25 15:43:47 +0100

    solver - solution

--- practice/vue-app/src/App.vue
+++ practice/vue-app/src/App.vue
@@ -1,30 +1,50 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import InputNumber from './components/InputNumber.vue'
+import ActionButton from './components/ActionButton.vue'
+
+import { add, subtract, multiply, divide } from './utils/calculate'
+
+import { ref } from 'vue'
+
+const firstNumber = ref<number>(0)
+const secondNumber = ref<number>(0)
+const result = ref<number | string>(0)
+
+const calculate = (operation: (a: number, b: number) => number) => {
+  try {
+    result.value = operation(firstNumber.value, secondNumber.value)
+  } catch (error: unknown) {
+    result.value = error.message
+  }
+}
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <div className="grid grid-cols-2 gap-x-4">
+      <InputNumber v-model="firstNumber" />
+      <InputNumber v-model="secondNumber" />
+    </div>
+    <div className="grid grid-cols-4 gap-x-4 my-4">
+      <ActionButton
+        label="+"
+        @click="calculate(add)"
+      />
+      <ActionButton
+        label="-"
+        @click="calculate(subtract)"
+      />
+      <ActionButton
+        label="*"
+        @click="calculate(multiply)"
+      />
+      <ActionButton
+        label="/"
+        @click="calculate(divide)"
+      />
+    </div>
+    <div>Result: {{ result }}</div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
---
+++ practice/vue-app/src/components/InputNumber.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const model = defineModel()
+</script>
+
+<template>
+  <input
+    type="number"
+    className="rounded-md shadow-md p-4"
+    v-model="model"
+  />
+</template>
+
+<style scoped></style>
