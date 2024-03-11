90bdec64f9b67621d8f8eb2e0c054360a5300697
Piotr <lewczykpiotr@gmail.com>
2024-03-11 13:39:18 +0100

    Add axios request and response interceptors to log request time

--- examples/module1/lesson4/axios-interceptor/index.js
+++ examples/module1/lesson4/axios-interceptor/index.js
@@ -2,17 +2,22 @@ import axios from 'axios';
 
 // Add a request interceptor
 axios.interceptors.request.use(function (config) {
+  config.metadata = { startTime: new Date() };
   return config;
 });
 
 // Add a response interceptor
 axios.interceptors.response.use(function (response) {
-  // Do something with response data
+  const {config: {url, metadata}} = response;
+  
+  const endTime = new Date();
+  const requestTime = endTime - metadata.startTime;
+
+  console.log(`Request to ${url} completed in ${requestTime}ms`);
+
   return response;
 });
 
-const {
-  data: { articles },
-} = await axios.get('/api/data/articles?timeout=3000');
+const {  data: { articles }} = await axios.get('/api/data/articles?timeout=3000');
 
 document.querySelector('#data').innerHTML = articles[0].content;
