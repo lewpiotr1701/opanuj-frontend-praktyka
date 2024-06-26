0fb1b9f06d3cedbc87be4e310d89b7de07dcb068
Piotr <lewczykpiotr@gmail.com>
2024-06-26 21:08:07 +0200

    chore: Refactor fetchWithTimeout so that timeout is cleared

--- practice/vue-app/src/utils/clientHTTP.ts
+++ practice/vue-app/src/utils/clientHTTP.ts
@@ -1,8 +1,14 @@
 export const fetchWithTimeout = (url, timeout) => {
-  return Promise.race([
-    fetch(url),
-    new Promise((_, reject) =>
-      setTimeout(() => reject(new Error('Timeout')), timeout)
-    )
-  ]);
+  let timeoutId;
+
+  const fetchPromise = fetch(url).finally(() => {
+    console.log('clear timeoutId',timeoutId)
+    clearTimeout(timeoutId)});
+
+  const timeoutPromise = new Promise((_, reject) => {
+    timeoutId = setTimeout(() => reject(new Error('Timeout')), timeout);
+    console.log('initialize timeoutId',timeoutId)
+  });
+
+  return Promise.race([fetchPromise, timeoutPromise]);
 };
